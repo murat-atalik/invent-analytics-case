a4950d70dd953e92d492963091f1358267402588
Murat ATALIK <62357472+murat-atalik@users.noreply.github.com>
2025-01-21 14:27:44 +0300

    feat: add ErrorItem and SkeletonLoader components; implement PaginatedList for movie display with loading and error handling

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,6 @@ export * from "./SearchInput/index";
 export * from "./SelectBox/index";
 export * from "./SearchField/index";
 export * from "./YearPicker/index";
+export * from "./PaginatedList/index";
+export * from "./ErrorItem/index";
+export * from "./MovieCard/index";
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,20 +1,9 @@
-import { shallowEqual } from "react-redux";
-import { SearchField } from "../../components";
-import { useAppSelector, useSearchMovie } from "../../hooks";
+import { PaginatedList, SearchField } from "../../components";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import image from "../../assets/backgrounds/back.png";
 import "./homePage.scss";
-import { MovieCard } from "../../components/MovieCard";
 
 export const HomePage = () => {
-  const searchKey = useAppSelector(
-    (s) => s.search?.lastSearchKey,
-    shallowEqual
-  );
-  const movies = useAppSelector(
-    (s) => s.search.searchList?.[searchKey ?? "-"]?.data.result?.Search ?? [],
-    shallowEqual
-  );
   return (
     <div className="homepage">
       <div className="homepage__header">
@@ -31,12 +20,7 @@ export const HomePage = () => {
           <SearchField />
         </div>
       </div>
-
-      <div className="homepage__movies">
-        {movies.map((movie, index) => (
-          <MovieCard key={movie.imdbID} {...movie} />
-        ))}
-      </div>
+      <PaginatedList />
     </div>
   );
 };
